55fb37b43779a7c368060ee33a6d59bd2c165bc6
siyeons <siyeonleeme@gmail.com>
2019-11-06 09:52:45 +0900

    like 라우팅을 Recruit로 수정

--- crew-server/routes/api/crew/sports/index.js
+++ crew-server/routes/api/crew/sports/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-router.use('/likes', require('./likes'))
+router.use('/recruit', require('./recruit'))
 router.user('/', require('./sports'))
 
 module.exports = router
