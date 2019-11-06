91c17efd4f8ed45c708aeb2a1a092ac237bfcfe9
siyeons <siyeonleeme@gmail.com>
2019-11-06 09:49:38 +0900

    기본 모듈

---
+++ crew-server/module/responseMessage.js
@@ -0,0 +1,35 @@
+// responseMessage.js
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+    SIGN_UP_SUCCESS: "회원가입 성공",
+    SIGN_UP_FAIL: "회원 가입 실패",
+    SIGN_IN_SUCCESS: "로그인 성공",
+    SIGN_IN_FAIL: "로그인 실패",
+    ALREADY_ID: "존재하는 ID 입니다.",
+    NO_USER: "존재하지 않는 유저 id 입니다.",
+    MISS_MATCH_PW: "비밀번호가 일치하지 않습니다",
+
+    // QUIZ_CREATE_SUCCESS: "퀴즈 작성 성공",
+    // QUIZ_CREATE_FAIL: "퀴즈 작성 실패",
+    QUIZ_READ_ALL_SUCCESS: "퀴즈 전체 조회 성공",
+    QUIZ_READ_ALL_FAIL: "퀴즈 전체 조회 성공",
+    QUIZ_READ_SUCCESS: "퀴즈 조회 성공",
+    QUIZ_READ_FAIL: "퀴즈 조회 성공",
+    // QUIZ_UPDATE_SUCCESS: "퀴즈 수정 성공",
+    // QUIZ_UPDATE_FAIL: "퀴즈 수정 실패",
+    // QUIZ_DELETE_SUCCESS: "퀴즈 삭제 성공",
+    // QUIZ_DELETE_FAIL: "퀴즈 삭제 실패",
+    QUIZ_CORRECT: "맞았습니다",
+    QUIZ_WRONG: "틀렸습니다",
+    ALREADY_ID: "존재하는 ID 입니다.",
+    NO_USER: "존재하지 않는 유저 입니다.",
+    NO_QUIZ: "존재하지 않는 퀴즈 입니다.",
+    MISS_MATCH_PW: "비밀번호가 일치하지 않습니다",
+    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+
+    NO_RANK_USER: "랭크에 존재하지 않는 유저입니다.",
+    NO_RANKERS: "랭크에 유저가 존재하지 않습니다.",
+    RANK_READ_SUCCESS: "랭크 조회 성공"
+
+}
