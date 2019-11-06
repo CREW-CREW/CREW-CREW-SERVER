c73400143841fc6639d3127ed100099fa15a90bc
siyeons <siyeonleeme@gmail.com>
2019-11-07 01:50:11 +0900

    Add user model

---
+++ crew-server/model/user.js
@@ -0,0 +1,77 @@
+const authUtil = require('../module/authUtil');
+const code = require('../module/statusCode');
+const msg = require('../module/responseMessage');
+const pool = require('../module/poolAsync');
+const encrypt = require('../module/encryption');
+const util = require('../module/util');
+const jwt = require('../module/jwt')
+
+module.exports = {
+    signin: ({
+        id,
+        password
+    }) => {
+        const table = 'user';
+        const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
+        return pool.queryParam_None(query)
+            .then(async (userResult) => {
+                console.log(userResult);
+                if (userResult.length == 0) {
+                    return {
+                        code: code.BAD_REQUEST,
+                        json: util.successFalse(msg.NO_USER)
+                    };
+                }
+                const user = userResult[0];
+                // 비밀번호 체크
+                const {
+                    hashed
+                } = await encrypt.encryptWithSalt(password, user.salt);
+                if (user.password != hashed) {
+                    return {
+                        code: code.BAD_REQUEST,
+                        json: util.successFalse(msg.MISS_MATCH_PW)
+                    };
+                }
+
+                const data = jwt.sign(user);
+                // 로그인 성공
+                return {
+                    code: code.OK,
+                    json: util.successTrue(msg.SIGN_IN_SUCCESS, data)
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
+    },
+    signup: ({
+        id,
+        password,
+        salt,
+        userName,
+        nickname,
+        area,
+        interest
+    }) => {
+        const table = 'user';
+        const fields = 'id, password, salt, userName, nickname, area, interest';
+        const questions = `?, ?, ?, ?, ?, ?, ?`;
+        const values = [id, password, salt, userName, nickname, area, interest];
+        return pool.queryParam_Parse(`INSERT INTO ${table}(${fields}) VALUES(${questions})`, values)
+            .catch(err => {
+                // ER_DUP_ENTRY
+                if(err.errno == 1062){
+                    console.log(err.errno, err.code);
+                    return {
+                        code: code.BAD_REQUEST,
+                        json: util.successFalse(msg.ALREADY_ID)
+                    };
+                }
+                console.log(err);
+                throw err;
+            });
+    },
+
+};
