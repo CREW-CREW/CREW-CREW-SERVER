74aecfaac043024e2f8a358c34f33bec90bfde8c
siyeons <siyeonleeme@gmail.com>
2019-12-03 04:21:13 +0900

    각 크루 디테일 보여주기

--- crew-server/routes/crews/crews.js
+++ crew-server/routes/crews/crews.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const crew = require('../../model/crew');
+const Crew = require('../../model/crew');
 const parameterChecker = require('../../module/parameterCheck');
 const code = require('../../module/statusCode')
 const util = require('../../module/util')
@@ -24,7 +24,7 @@ router.post('/list', (req, res) => {
             code: code.INTERNAL_SERVER_ERROR,
             json: util.successFalse(msg.INTERNAL_SERVER_ERROR)
         };
-    }).then(({code, json})=>{
+    }).then(()=>{
         res.status(code).send(json)
     });
 });
@@ -56,25 +56,25 @@ router.get('/:crewIdx/recruit', authUtil.isLoggedin , (req, res) => {
 // 크루 상세 정보 페이지 
 router.get('/:crewIdx', (req, res) => {
     const crewIdx = req.params.crewIdx;
-
-    crew.read({crewIdx})
-    .then(result => {
-        console.log(result);
-        if(result.code && result.json) return result;
-        const crewSingle = result[0];
-        return {
-            code: code.OK,
-            json: util.successTrue('', crewSingle)
-        };
+    if(!crewIdx){
+        res.status(code.BAD_REQUEST)
+        .send(util.successFalse(msg.NULL_VALUE));
+        return;
+    }
+    Crew.read({crewIdx})
+    .then((Crew) => {
+        const {crewName, category, level, time, content, image} = Crew[0];
+        //res.status(code.OK).send(util.successTrue(msg.CREW_READ_SUCCESS, data));
+        res.render('crews/crewDetail', {crewName, category, level, time, content, image}); 
     }).catch(err => {
         console.log(err);
-        return {
-            code: code.INTERNAL_SERVER_ERROR,
-            json: util.successFalse(msg.INTERNAL_SERVER_ERROR)
-        };
-    }).then(({code, json})=>{
-        res.status(code).send(json)
-    });
+        res.status(code.BAD_REQUEST)
+        .send(util.successFalse(msg.OUT_OF_VALUE));
+    })
+    // }).then(({code, json})=>{
+    //   //  res.status(code).send(json)
+    //     res.render('crews/crewDetail', {crewName:crewName, category:category, level:level, time:time, content:content, image:image}); 
+    // });
 });
 
 // 크루 새로 생성하기 
